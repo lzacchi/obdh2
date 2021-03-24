d126a02585dd778c26636e3ee31a46ebbb62e167
lzacchi <zacchilucasm@gmail.com>
2021-03-24 12:45:48 -0300

    Fix path error

--- .github/workflows/run_all_tests.sh
+++ .github/workflows/run_all_tests.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# cd ../../../.github/workflows/
-# cd ../../firmware/tests/devices/
-
-./../../firmware/tests/devices/current_sensor_unit_test | grep -q "FAILED" && { exit 255; }
-
-# echo "PASSED"
+firmware/tests/devices/temp_sensor_unit_test | grep -q "FAILED" && { exit 255; }
