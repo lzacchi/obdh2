b19f5d70f6dbd1969663275295701948348cc1b5
lzacchi <zacchilucasm@gmail.com>
2021-03-24 12:35:19 -0300

    Remove cd command from bash script

--- .github/workflows/run_all_tests.sh
+++ .github/workflows/run_all_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # cd ../../../.github/workflows/
-cd ../../firmware/tests/devices/
+# cd ../../firmware/tests/devices/
 
 ./current_sensor_unit_test | grep -q "FAILED" && { echo "There were failed tests"; exit 1; }
 
