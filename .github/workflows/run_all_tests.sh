e1ba2015d32684f8633c6fd3fac62d2e7fb64a69
lzacchi <zacchilucasm@gmail.com>
2021-03-24 12:41:43 -0300

    Change working directories, execution path

--- .github/workflows/run_all_tests.sh
+++ .github/workflows/run_all_tests.sh
@@ -3,6 +3,6 @@
 # cd ../../../.github/workflows/
 # cd ../../firmware/tests/devices/
 
-./current_sensor_unit_test | grep -q "FAILED" && { exit 255; }
+./../../firmware/tests/devices/current_sensor_unit_test | grep -q "FAILED" && { exit 255; }
 
 # echo "PASSED"
